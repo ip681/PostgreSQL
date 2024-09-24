5a11d762790202a77d063750cda54af62f395ab8
Ivo Petkov <ipetkov.com@gmail.com>
2024-09-25 00:12:08 +0300

    Create 12.salary.sql

---
+++ 02_Basic_CRUD/exercise/12.salary.sql
@@ -0,0 +1,14 @@
+SELECT
+    concat(
+    first_name,
+    ' ',
+    last_name
+    ) AS full_name,
+    job_title,
+    salary
+FROM 
+    employees
+WHERE
+    salary IN (12500, 14000, 23600, 25000)
+ORDER BY
+    salary DESC;
