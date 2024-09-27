c04823ac8570444a9be5cc31481725f58fdaa0cc
Ivo Petkov <ipetkov.com@gmail.com>
2024-09-27 19:58:07 +0300

    Create 15.new_table.sql

---
+++ 02_Basic_CRUD/exercise/15.new_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE company_chart 
+AS
+SELECT
+    concat(
+    first_name,
+    ' ',
+    last_name
+    ) AS full_name,
+    job_title,
+    department_id,
+    manager_id
+FROM 
+    employees;
