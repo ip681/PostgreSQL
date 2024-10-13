825c96c2570dddb8f23d8096716166f6e4bdc4dc
Ivo Petkov <ipetkov.com@gmail.com>
2024-10-14 01:05:03 +0300

    Create 02.select_employees_by_filtering.sql

---
+++ 02_Basic_CRUD/lab/02.select_employees_by_filtering.sql
@@ -0,0 +1,14 @@
+SELECT
+    id,
+    concat_ws(
+    ' ',
+    first_name,
+    last_name
+    ) AS full_name,
+    job_title,
+    salary
+FROM employees
+WHERE
+    salary > 1000.00
+ORDER BY id
+;
