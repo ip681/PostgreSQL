0deec8b4453865655e7e3f5dc5ae1e86387ea6f6
Ivo Petkov <ipetkov.com@gmail.com>
2024-10-14 22:09:09 +0300

    Create 03.select_employees_by_multiple_filters.sql

---
+++ 02_Basic_CRUD/lab/03.select_employees_by_multiple_filters.sql
@@ -0,0 +1,15 @@
+SELECT
+    id,
+    first_name,
+    last_name,
+    job_title,
+    department_id,
+    salary
+FROM employees
+WHERE
+    department_id = 4
+    AND
+    salary >= 1000
+ORDER BY 
+    id
+;
