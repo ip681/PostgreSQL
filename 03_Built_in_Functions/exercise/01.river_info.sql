6709f7b2f80a2cf1a5b84ace320b489870ddb625
Ivo Petkov <ipetkov.com@gmail.com>
2024-10-31 00:51:09 +0200

    Create 01.river_info.sql

---
+++ 03_Built_in_Functions/exercise/01.river_info.sql
@@ -0,0 +1,18 @@
+CREATE VIEW
+    view_river_info
+AS
+SELECT
+    concat_ws(
+    ' ',
+    'The river',
+    river_name,
+    'flows into the',
+    outflow,
+    'and is',
+    length,
+    'kilometers long.'
+    ) AS "River Information"
+FROM
+    rivers
+ORDER BY
+    river_name;
