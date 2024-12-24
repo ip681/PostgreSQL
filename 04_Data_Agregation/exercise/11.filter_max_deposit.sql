86e753d99feebebe80099c7956fe4c0d99206ca9
Ivo Petkov <ipetkov.com@gmail.com>
2024-12-24 02:42:45 +0200

    Create 11.filter_max_deposit.sql

---
+++ 04_Data_Agregation/exercise/11.filter_max_deposit.sql
@@ -0,0 +1,12 @@
+SELECT
+    magic_wand_creator,
+    MAX(deposit_amount) AS max_deposit_amount
+FROM
+    wizard_deposits
+GROUP BY
+    magic_wand_creator
+HAVING
+    MAX(deposit_amount) NOT BETWEEN 20000 AND 40000
+ORDER BY
+    max_deposit_amount DESC
+LIMIT 3;
