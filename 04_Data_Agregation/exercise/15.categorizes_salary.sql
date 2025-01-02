f4ce7ce16487f9734767cfc4a2d8d6fc9ccc3e3f
Ivo Petkov <ipetkov.com@gmail.com>
2025-01-02 16:58:18 +0200

    Create 15.categorizes_salary.sql

---
+++ 04_Data_Agregation/exercise/15.categorizes_salary.sql
@@ -0,0 +1,14 @@
+SELECT
+    job_title,
+    CASE
+        WHEN AVG(salary) > 45800 THEN 'Good'
+        WHEN AVG(salary) BETWEEN 27500 AND 45800 THEN 'Medium'
+        ELSE 'Need Improvement'
+    END AS category
+FROM
+    employees
+GROUP BY
+    job_title
+ORDER BY
+    category,
+    job_title;
