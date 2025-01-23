080496fd199054cac41279b8acf743dba215544c
Ivo Petkov <ipetkov.com@gmail.com>
2025-01-23 17:39:45 +0200

    Create 01.primary_key.sql

---
+++ 05_Table_Relations/exercise/01.primary_key.sql
@@ -0,0 +1,14 @@
+CREATE TABLE products (
+    product_name VARCHAR(100)
+);
+
+INSERT INTO products (product_name)
+VALUES
+    ('Broccoli'),
+    ('Shampoo'),
+    ('Toothpaste'),
+    ('Candy');
+
+ALTER TABLE products
+ADD COLUMN
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
