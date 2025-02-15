102f29c610980559844e82b6a7d5be2268fab428
Ivo Petkov <ipetkov.com@gmail.com>
2025-02-15 02:33:50 +0200

    Create 04.delete_mountains.sql

---
+++ 05_Table_Relations/lab/04.delete_mountains.sql
@@ -0,0 +1,13 @@
+CREATE TABLE mountains(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE peaks (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50),
+    mountain_id INT,
+    CONSTRAINT fk_mountain_id
+        FOREIGN KEY (mountain_id) REFERENCES mountains(id)
+            ON DELETE CASCADE
+);
