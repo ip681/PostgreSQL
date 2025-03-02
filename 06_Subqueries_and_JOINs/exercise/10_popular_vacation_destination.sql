2f22cce7ac80b810bdf057300852c61413067480
Ivo Petkov <ipetkov.com@gmail.com>
2025-03-03 01:13:56 +0200

    Create 10_popular_vacation_destination.sql

---
+++ 06_Subqueries_and_JOINs/exercise/10_popular_vacation_destination.sql
@@ -0,0 +1,15 @@
+SELECT
+    a.country,
+    COUNT(b.booking_id) AS booking_count
+FROM
+    apartments AS a
+        JOIN bookings AS b
+            USING (apartment_id)
+WHERE
+    b.booked_at >= '2021-05-18 07:52:09.904+03'
+    AND
+    b.booked_at < '2021-09-17 19:48:02.147+03'
+GROUP BY
+    a.country
+ORDER BY
+    booking_count DESC;
