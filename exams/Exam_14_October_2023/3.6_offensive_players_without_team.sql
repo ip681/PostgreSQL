600fceca7122029424563f5501850ba80b1e4593
Ivo Petkov <ipetkov.com@gmail.com>
2025-04-28 21:39:26 +0300

    Create 3.6_offensive_players_without_team.sql

---
+++ exams/Exam_14_October_2023/3.6_offensive_players_without_team.sql
@@ -0,0 +1,18 @@
+SELECT
+    p.id,
+    CONCAT(p.first_name, ' ', p.last_name) AS full_name,
+    p.age,
+    p.position,
+    p.salary,
+    sd.pace,
+    sd.shooting
+FROM
+    players AS p
+    JOIN skills_data AS sd
+        ON p.skills_data_id = sd.id
+WHERE
+    p.position = 'A'
+    AND
+    p.team_id IS NULL
+    AND
+    sd.pace + sd.shooting > 130;
