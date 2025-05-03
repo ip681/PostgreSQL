2b2309ed9bcbec327db81985e31ec784844a712b
Ivo Petkov <ipetkov.com@gmail.com>
2025-05-03 09:53:40 +0300

    Create 3.7_teams_with_player_count_and_fan_base.sql

---
+++ exams/Exam_14_October_2023/3.7_teams_with_player_count_and_fan_base.sql
@@ -0,0 +1,18 @@
+SELECT
+    t.id AS team_id,
+    t.name,
+    COUNT(p.id) AS player_count,
+    t.fan_base
+FROM
+    teams AS t
+    LEFT JOIN players AS p
+        ON t.id = p.team_id
+WHERE
+    t.fan_base > 30000
+GROUP BY
+    t.id,
+    t.name,
+    t.fan_base
+ORDER BY
+    player_count DESC,
+    fan_base DESC;
